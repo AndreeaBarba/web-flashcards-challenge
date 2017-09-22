83d8e7929230bee791a518e4f194f047a11d837c
Kara Kelley <kelley.15@gmail.com>
2017-09-22 17:53:08 -0500

    Create deck route and round routes for cards.

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,10 +1,11 @@
 
 get '/rounds/:round_id/cards/:card_id' do
-  @deck = Deck.find(params[:round_id])
+  @round = Round.find(params[:round_id])
+  # @deck = Round.find(params[:round_id]).deck
   @card = Card.find(params[:card_id])
   erb :'cards/show'
 end
 
 post '/rounds/:round_id/cards/:card_id' do
-  @guess = Guess.create(:user_answer => params[:user_answer, :card_id => params[:card_id]
+  @guess = Guess.create(:user_answer => params[:user_answer], :card_id => params[:card_id])
 end
