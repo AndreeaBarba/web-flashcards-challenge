10cf1eb4cfd586e4b6920896cf1e619b949bc157
Mitch Mele <mitchmele@gmail.com>
2017-09-22 17:42:19 -0500

    Create feedback feature and fix guess model, but hard-coded round ID.

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,3 @@
-
 get '/rounds/:round_id/cards/:card_id' do
   @deck = Deck.find(params[:round_id])
   @card = Card.find(params[:card_id])
@@ -6,5 +5,14 @@ get '/rounds/:round_id/cards/:card_id' do
 end
 
 post '/rounds/:round_id/cards/:card_id' do
-  @guess = Guess.create(:user_answer => params[:user_answer, :card_id => params[:card_id]
+  guess = Guess.create(:user_answer => params[:user_answer], :card_id => params[:card_id], :round_id => 1)
+  ep guess
+  if guess.user_answer == guess.card.answer
+    guess.correct? == true
+    @message = "You are correct!"
+  else
+    guess.correct? == false
+    @message = "You are incorrect! The answer is: #{guess.card.answer}"
+  end
+  erb :"/cards/feedback"
 end
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,5 +1,5 @@
 class Guess < ApplicationRecord
   belongs_to :card
   belongs_to :round
-  belongs_to :user, through: :round
+  belongs_to :user, foreign_key: :user_id
 end
