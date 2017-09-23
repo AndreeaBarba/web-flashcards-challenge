5518bf2fb12f7eba2e80887687427c4ddb328265
Kara Kelley <kelley.15@gmail.com>
2017-09-23 16:05:16 -0500

    Go through all cards (randomized) in one round.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -14,14 +14,12 @@ end
 get '/decks/:id' do
   found_deck = Deck.find(params[:id])
   card_count = found_deck.cards.count
-  ep cards = (1..card_count).to_a
-  ep shuffled_cards = cards.shuffle
-  @round = Round.create(deck_id: params[:id], user_id:
+  cards = (1..card_count).to_a
+  shuffled_cards = cards.shuffle
+  round = Round.create(deck_id: params[:id], user_id:
     session[:user_id])
-  ep @round
-  session[:round] = { @round.id => shuffled_cards }
-  session[:curr_index] = 0
-  ep session
-  session[:current_card] = session[:round][@round.id][session[:curr_index]]
-  redirect "/rounds/#{@round.id}/cards/#{session[:current_card]}"
+  session[:cards] = shuffled_cards
+  session[:counter] = 0
+  redirect "/rounds/#{round.id}/cards/#{session[:cards][0]}?round_id=#{round.id}"
 end
+
